ebf86d9b2a3fb08dacc20d113544abca3986e987
Hatairat titnam <akikeedum@gmail.com>
2024-11-25 14:58:57 +0700

    question2,3

--- question3-4.dart
+++ question3-4.dart
@@ -4,12 +4,27 @@ double calculateArea({double length = 1, double width = 1}) {
   return length * width; // คำนวณพื้นที่ของสี่เหลี่ยมผืนผ้า
 }
 
-void main() {
-  stdout.write("กรุณากรอกความยาว : ");
-  double length = double.parse(stdin.readLineSync()!); // รับค่าความยาวจากผู้ใช้
+double getPositiveInput(String prompt) {
+  double value;
+  while (true) {
+    stdout.write(prompt);
+    String? input = stdin.readLineSync();
+    if (input != null && input.isNotEmpty) {
+      value = double.tryParse(input) ?? -1; // ใช้ tryParse เพื่อตรวจสอบว่าเป็นตัวเลขไหม
+      if (value > 0) {
+        return value; // ถ้าค่าที่กรอกเป็นค่าบวกจะกลับค่าที่กรอก
+      } else {
+        print("กรุณากรอกค่ามากกว่า 0");
+      }
+    } else {
+      return 1; // ถ้าไม่ได้กรอกอะไร จะใช้ค่าเริ่มต้นเป็น 1
+    }
+  }
+}
 
-  stdout.write("กรุณากรอกความกว้าง : ");
-  double width = double.parse(stdin.readLineSync()!); // รับค่าความกว้างจากผู้ใช้
+void main() {
+  double length = getPositiveInput("กรุณากรอกความยาว : ");
+  double width = getPositiveInput("กรุณากรอกความกว้าง : ");
 
   double area = calculateArea(length: length, width: width);
   print("พื้นที่ของสี่เหลี่ยมผืนผ้า คือ $area");
