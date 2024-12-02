076292009658fe3eec83743c4d1ed734cc4208b9
Hatairat titnam <akikeedum@gmail.com>
2024-12-02 16:15:10 +0700

    question4,6

--- question6-6.dart
+++ question6-6.dart
@@ -1,19 +1,40 @@
 abstract class Bottle {
   void open();
-
-  factory Bottle() {
-    return CokeBottle();
+ 
+  // Factory constructor with conditional logic
+  factory Bottle(String type) {
+    if (type == "Coke") {
+      return CokeBottle();
+    } else if (type == "Pepsi") {
+      return PepsiBottle();
+    } else {
+      throw ArgumentError("Invalid bottle type");
+    }
   }
 }
-
+ 
+// CokeBottle implements Bottle
 class CokeBottle implements Bottle {
   @override
   void open() {
     print("Coke bottle is opened");
   }
 }
+ 
+// PepsiBottle implements Bottle
+class PepsiBottle implements Bottle {
+  @override
+  void open() {
+    print("Pepsi bottle is opened");
+  }
+}
 
 void main() {
-  Bottle bottle = Bottle(); 
-  bottle.open();
+  // Create a CokeBottle using the factory constructor
+  Bottle cokeBottle = Bottle("Coke");
+  print(cokeBottle);  // This will print: Instance of 'CokeBottle'
+  
+  // Create a PepsiBottle using the factory constructor
+  Bottle pepsiBottle = Bottle("Pepsi");
+  pepsiBottle.open();  // This will print: Pepsi bottle is opened
 }
